0b23e1cfbd5f8166a743a0c626eda7a9aca5e82e
Kevin Cổ <kevin.co@protonmail.com>
2023-02-14 10:57:44 +0000

    Cache sum of paths as they are being built

--- MinTrianglePath.scala
+++ MinTrianglePath.scala
@@ -5,6 +5,10 @@ object MinTrianglePath {
 
   type Error = String
 
+  private final case class Path(nodes: Vector[Int]) {
+    val sum: Int = nodes.sum
+  }
+
   private def readNextLineOfInts(): Either[Error, Vector[Int]] = {
     try {
       val line = readLine()
@@ -16,20 +20,20 @@ object MinTrianglePath {
   }
 
   @tailrec
-  private def pathsFromStdIn(acc: Map[Vector[Int], Int] = Map.empty, lineNumber: Int = 1): Either[Error, List[List[Int]]] = {
+  private def pathsFromStdIn(acc: Map[Path, Int] = Map.empty, lineNumber: Int = 1): Either[Error, List[Path]] = {
     readNextLineOfInts() match {
-      case Right(IndexedSeq()) => Right(acc.keys.map(_.toList).toList)
+      case Right(IndexedSeq()) => Right(acc.keys.toList)
       case Right(line) =>
         if (line.length != lineNumber)
           Left(s"$lineNumber integers expected but ${line.length} found: $line")
         else {
           if (lineNumber == 1)
-            pathsFromStdIn(Map(line -> 0), lineNumber + 1)
+            pathsFromStdIn(Map(Path(line) -> 0), lineNumber + 1)
           else {
-            val appended = acc.flatMap { case (path, idx) => Map((path :+ line(idx)) -> idx, (path :+ line(idx + 1)) -> (idx + 1))}
+            val appended = acc.flatMap { case (path, idx) => Map(path.copy(path.nodes :+ line(idx)) -> idx, path.copy(path.nodes :+ line(idx + 1)) -> (idx + 1)) }
             val filteredForShortest = appended
-              .groupBy{ case (_, idx) => idx}
-              .map { case (_, pathWithLastNumIdx) => pathWithLastNumIdx.minBy{ case (path, _) => path.sum}}
+              .groupBy { case (_, idx) => idx }
+              .map { case (_, pathWithLastNumIdx) => pathWithLastNumIdx.minBy { case (path, _) => path.sum } }
             pathsFromStdIn(filteredForShortest, lineNumber + 1)
           }
         }
@@ -42,7 +46,7 @@ object MinTrianglePath {
       case Left(error) => println(error)
       case Right(paths) =>
         val shortestPath = paths.minBy(_.sum)
-        println(s"Minimal path is ${shortestPath.mkString(" + ")} = ${shortestPath.sum}")
+        println(s"Minimal path is ${shortestPath.nodes.mkString(" + ")} = ${shortestPath.sum}")
     }
   }
 }
